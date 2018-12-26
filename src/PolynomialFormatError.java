c2a5ded0f9ca43c18b31e75535972f8ad3c7a7a0
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2018-12-26 16:48:32 -0500

    Error class added
    Signed-off-by: Mani9723 <38226555+Mani9723@users.noreply.github.com>

---
+++ src/PolynomialFormatError.java
@@ -0,0 +1,18 @@
+/**
+ * PolynomialEvaluation
+ *
+ * @author Mani Shah
+ * Date:  12/26/2018
+ * Time:  16:47
+ * @since 1.0
+ */
+
+public class PolynomialFormatError extends Exception
+{
+	PolynomialFormatError(){
+		super();
+	}
+	PolynomialFormatError(String message){
+		super(message);
+	}
+}
