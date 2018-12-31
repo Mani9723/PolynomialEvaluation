c1fc59ff389b316eb8d30b21299f592ae708afb1
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2018-12-30 19:57:56 -0500

    Add Junit 4 test class
    Signed-off-by: Mani9723 <38226555+Mani9723@users.noreply.github.com>

---
+++ JUnit4Test/PolynomialTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+/**
+ * PolynomialEvaluation
+ *
+ * @author Mani Shah
+ * Date:  12/30/2018
+ * Time:  19:57
+ * @since 1.0
+ */
+public class PolynomialTest
+{
+
+}
