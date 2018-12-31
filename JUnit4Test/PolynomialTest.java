e4da6a4e8127857ff2f731c27848b514b8d5b5ae
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2018-12-30 20:37:24 -0500

    Test Cases added
    Signed-off-by: Mani9723 <38226555+Mani9723@users.noreply.github.com>

--- JUnit4Test/PolynomialTest.java
+++ JUnit4Test/PolynomialTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,5 +12,57 @@ import static org.junit.Assert.*;
  */
 public class PolynomialTest
 {
+	private Polynomial polynomial;
+
+	@Test
+	public void testPoly1()
+	{
+		String poly = "2x^3+1234+4x^98";
+		polynomial = new Polynomial(poly);
+		assertEquals(poly,polynomial.toString());
+		polynomial.deleteList();
+	}
+	@Test
+	public void testPoly2()
+	{
+		String poly = "5x^12-2x^6+x^5-198x+1";
+		polynomial = new Polynomial(poly);
+		assertEquals(poly,polynomial.toString());
+		polynomial.deleteList();
+	}
+	@Test
+	public void testPoly3()
+	{
+		String poly = "x^4-x^3-x^2-x-1";
+		polynomial = new Polynomial(poly);
+		assertEquals(poly,polynomial.toString());
+		System.out.println(polynomial);
+		polynomial.deleteList();
+	}
+	@Test
+	public void testPoly4()
+	{
+		String poly = "12x^3+123-33x^5";
+		polynomial = new Polynomial(poly);
+		assertEquals(poly,polynomial.toString());
+		polynomial.deleteList();
+	}
+	@Test
+	public void testPoly5()
+	{
+		String poly = "-4x^98";
+		polynomial = new Polynomial(poly);
+		assertEquals(poly,polynomial.toString());
+		polynomial.deleteList();
+	}
+	@Test
+	public void testPoly6()
+	{
+		String poly = "2x";
+		polynomial = new Polynomial(poly);
+		assertEquals(poly,polynomial.toString());
+		polynomial.deleteList();
+	}
+
 
 }
