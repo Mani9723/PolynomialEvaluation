e791fb299d6b00e4ff1acf75206131a31ba34b25
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2019-01-03 09:31:26 -0500

    Updated Test Cases
    Signed-off-by: Mani9723 <38226555+Mani9723@users.noreply.github.com>

--- JUnit4Test/PolynomialTest.java
+++ JUnit4Test/PolynomialTest.java
@@ -17,9 +17,9 @@ public class PolynomialTest
 	@Test
 	public void testPoly1()
 	{
-		String poly = "2x^3+1234+4x^98";
+		String poly = "2x^3+1234-4x^98";
 		polynomial = new Polynomial(poly);
-		assertEquals(poly,polynomial.toString());
+		assertEquals("-4x^98+2x^3+1234",polynomial.toString());
 		polynomial.deletePolynomial();
 	}
 	@Test
@@ -33,9 +33,9 @@ public class PolynomialTest
 	@Test
 	public void testPoly3()
 	{
-		String poly = "x^4-x^3-x^2-x-1";
+		String poly = "x^3-x^5-x^2-x^6-1";
 		polynomial = new Polynomial(poly);
-		assertEquals(poly,polynomial.toString());
+		assertEquals("-x^6-x^5+x^3-x^2-1",polynomial.toString());
 		polynomial.deletePolynomial();
 	}
 	@Test
@@ -43,7 +43,7 @@ public class PolynomialTest
 	{
 		String poly = "12x^3+123-33x^5";
 		polynomial = new Polynomial(poly);
-		assertEquals(poly,polynomial.toString());
+		assertEquals("-33x^5+12x^3+123",polynomial.toString());
 		polynomial.deletePolynomial();
 
 	}
@@ -69,7 +69,7 @@ public class PolynomialTest
 	{
 		String poly = "2x^3 - 4x+x- 23";
 		polynomial = new Polynomial(poly);
-		assertEquals("2x^3-4x+x-23",polynomial.toString());
+		assertEquals("2x^3+x-4x-23",polynomial.toString());
 		polynomial.deletePolynomial();
 	}
 	@Test
@@ -77,7 +77,7 @@ public class PolynomialTest
 	{
 		String poly = "2x^3 - 4x + x - 23";
 		polynomial = new Polynomial(poly);
-		assertEquals("2x^3-4x+x-23",polynomial.toString());
+		assertEquals("2x^3+x-4x-23",polynomial.toString());
 		polynomial.deletePolynomial();
 	}
 
@@ -86,7 +86,7 @@ public class PolynomialTest
 	{
 		String poly = "-x^2+23x+6x^2";
 		polynomial = new Polynomial(poly);
-		assertEquals(-1,polynomial.getTerm(0).coeff);
+		assertEquals(6,polynomial.getTerm(0).coeff);
 		polynomial.deletePolynomial();
 	}
 	@Test
@@ -94,7 +94,7 @@ public class PolynomialTest
 	{
 		String poly = "-x^2+23x+6x^4";
 		polynomial = new Polynomial(poly);
-		assertEquals(1,polynomial.getTerm(1).expo);
+		assertEquals(2,polynomial.getTerm(1).expo);
 		polynomial.deletePolynomial();
 	}
 	@Test
